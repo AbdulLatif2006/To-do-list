02ee839a258dc06e0c71b7c025455e2f61687347
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 <sayalatif731@gmail.com>
2025-04-24 08:47:27 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+  
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBX422jzTqH-zHufidDsbDUfx0USgkNvGk",
+  authDomain: "abdul-latif-30eb0.firebaseapp.com",
+  projectId: "abdul-latif-30eb0",
+  storageBucket: "abdul-latif-30eb0.firebasestorage.app",
+  messagingSenderId: "84187616371",
+  appId: "1:84187616371:web:c1b25857234824f0138606",
+  measurementId: "G-G54GZN3DHS"
+};
